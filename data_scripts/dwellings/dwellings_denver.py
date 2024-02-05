e9c8e78ec05102d4d52879eafff7865fbea3d861
weste <wes18014@byui.edu>
2024-02-05 16:38:28 -0700

    started work on downloading files from private google drive in python

--- data_scripts/dwellings/dwellings_denver.py
+++ data_scripts/dwellings/dwellings_denver.py
@@ -1,18 +1,21 @@
 # packages
 import pandas as pd
+import gdown
 
 # get and clean data
-url = "https://www.denvergov.org/media/gis/DataCatalog/real_property_sales_book_2013/csv/SalesBook_2013.csv"
-dat = (pd.read_csv(url)
-    .query('~COMMUSE.notna()')
-    .drop(
-        columns = ['Model', 'Cluster', 'Group', 
-            'CondoComplex', 'CondoComplexName', 'COMMUSE'])
-    .assign(
-        NBHD = lambda x: x.NBHD.astype('object'))
-    .query('LIVEAREA > 0')
-    .set_index('PARCEL')
-)
+url = "https://drive.google.com/file/d/uc?id=1Ac5zPy_8UZnI8gxxsnBs-45D_ZOUZ1PQ/"
+dat = gdown.download(url)
+
+# dat = (pd.read_csv(url)
+#     .query('~COMMUSE.notna()')
+#     .drop(
+#         columns = ['Model', 'Cluster', 'Group', 
+#             'CondoComplex', 'CondoComplexName', 'COMMUSE'])
+#     .assign(
+#         NBHD = lambda x: x.NBHD.astype('object'))
+#     .query('LIVEAREA > 0')
+#     .set_index('PARCEL')
+# )
 
 
 # rename columns to lower case
