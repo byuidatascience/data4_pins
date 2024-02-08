24dd82b5b5ba984916153ae3770c2d6f9ba99495
weste <wes18014@byui.edu>
2024-02-08 14:44:46 -0700

    starting google drive authentication

--- data_scripts/dwellings/dwellings_denver.py
+++ data_scripts/dwellings/dwellings_denver.py
@@ -1,22 +1,23 @@
 # packages
 import pandas as pd
-import gdown
+import pydrive2
+
 
 # get and clean data
 url = "https://drive.google.com/file/d/uc?id=1Ac5zPy_8UZnI8gxxsnBs-45D_ZOUZ1PQ/"
 
-dat = gdown.download(url)
 
-# dat = (pd.read_csv(url)
-#     .query('~COMMUSE.notna()')
-#     .drop(
-#         columns = ['Model', 'Cluster', 'Group', 
-#             'CondoComplex', 'CondoComplexName', 'COMMUSE'])
-#     .assign(
-#         NBHD = lambda x: x.NBHD.astype('object'))
-#     .query('LIVEAREA > 0')
-#     .set_index('PARCEL')
-# )
+
+dat = (pd.read_csv(url)
+    .query('~COMMUSE.notna()') # Filter nulls from commuse column
+    .drop(
+        columns = ['Model', 'Cluster', 'Group', 
+            'CondoComplex', 'CondoComplexName', 'COMMUSE']) # drop these columns
+    .assign(
+        NBHD = lambda x: x.NBHD.astype('object')) # Convert 'NBHD' column to object type
+    .query('LIVEAREA > 0') # Filter rows where 'LIVEAREA' is greater than 0
+    .set_index('PARCEL') # Set 'PARCEL' column as the index
+)
 
 
 # rename columns to lower case
