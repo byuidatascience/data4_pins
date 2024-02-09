b0f43f44eb2ad9d729334487b18e8a0f1106c67d
weste <wes18014@byui.edu>
2024-02-09 12:31:08 -0700

    maybe getting somewhere with the file reading

--- data_scripts/dwellings/dwellings_denver.py
+++ data_scripts/dwellings/dwellings_denver.py
@@ -10,8 +10,7 @@ from io import StringIO, BytesIO
 url = "https://drive.google.com/file/d/1i0ASVFW9t_oUpwD0gGoqabRakDQrWttY/view?usp=drive_link"
 
 # Set the drive to an authentication object with access to target account.
-drive = GoogleDrive(GoogleAuth)
-
+drive = GoogleDrive(GoogleAuth())
 
 # Parse URL.
 file_id = url.split('/')[2]
@@ -20,6 +19,10 @@ file = drive.CreateFile({'id':file_id})
 
 # ????!!?!?!
 # buffer = file.GetContentIOBuffer()
+
+pd.read_csv(
+    StringIO(file.GetContentString())
+)
 
 dat = (pd.read_csv(file.read_csv())
     .query('~COMMUSE.notna()') # Filter nulls from commuse column
