c6561fe51eda1b83aaad11be46e419885b69fc7e
dustydells <98501262+dustydells@users.noreply.github.com>
2024-03-13 15:02:27 -0600

    Making scripts for math 221. baby_boom qmd is not finished yet but the others are.

---
+++ data_scripts/baby_boom/baby_boom.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+baby_boom <- read_csv('https://github.com/byuistats/data/raw/master/BabyBoom-JSE/BabyBoom-JSE.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, baby_boom, type = "parquet")
+
+pin_name <- "baby_boom"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
