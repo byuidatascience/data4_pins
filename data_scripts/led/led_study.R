76bcaf5d113fe41537fbae7a4f5f02aeb4641894
dustydells <98501262+dustydells@users.noreply.github.com>
2024-03-12 20:10:26 -0600

    adding the code that publishes the data to the server

--- data_scripts/led/led_study.R
+++ data_scripts/led/led_study.R
@@ -6,3 +6,13 @@ google_file <- drive_ls(sdrive)  |>
 tempf <- tempfile()
 drive_download(google_file, tempf)
 led_study <- read_csv(tempf)
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, led_study, type = "parquet")
+
+pin_name <- "led_study"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
