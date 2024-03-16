e2f8220cb29b32961e32f573018976f325dc6a1a
dustydells <98501262+dustydells@users.noreply.github.com>
2024-03-16 13:48:54 -0600

    batting averages is done

---
+++ data_scripts/batting_averages/batting_averages.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+batting_averages <- read_csv('https://github.com/byuistats/data/raw/master/BattingAverages/BattingAverages.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, batting_averages, type = "parquet")
+
+pin_name <- "batting_averages"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
