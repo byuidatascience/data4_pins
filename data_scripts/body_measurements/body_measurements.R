3c9f6af61ca4eb5e768b26ad79979f87219283f5
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-19 16:39:03 -0600

    started body_measurements

---
+++ data_scripts/body_measurements/body_measurements.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+body_measurements <- read_csv('')
+# source to be put in qmd : http://lib.stat.cmu.edu/datasets/bodyfat
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, body_measurements, type = "parquet")
+
+pin_name <- "body_measurements"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
