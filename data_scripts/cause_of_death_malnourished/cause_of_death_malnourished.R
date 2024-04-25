4233baffbc1c0ddb4d854177b1b9f0432955b22a
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-25 15:56:17 -0600

    script and qmd done

---
+++ data_scripts/cause_of_death_malnourished/cause_of_death_malnourished.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+cause_of_death_malnourished <- read_csv('https://github.com/byuistats/data/raw/master/CauseOfDeath-Malnourished/CauseOfDeath-Malnourished.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, cause_of_death_malnourished, type = "parquet")
+
+pin_name <- "cause_of_death_malnourished"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
