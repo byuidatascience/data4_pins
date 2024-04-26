e75585621d6cd286b55806ba9a7d30fc573f9493
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-26 16:59:43 -0600

    script and qmd done

---
+++ data_scripts/comet_water_production_magnitude/comet_water_production_magnitude.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+comet_water_production_magnitude <- read_csv('https://github.com/byuistats/data/raw/master/Comet-WaterProduction-Magnitude/Comet-WaterProduction-Magnitude.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, comet_water_production_magnitude, type = "parquet")
+
+pin_name <- "comet_water_production_magnitude"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
