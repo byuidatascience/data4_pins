3fe3f355c6142de3ed2095dd3c03e96a39ff17c7
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-27 13:03:35 -0600

    script and qmd done

---
+++ data_scripts/dasl_fiber/dasl_fiber.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+dasl_fiber <- read_csv('https://github.com/byuistats/data/raw/master/DASL-Fiber/DASL-Fiber.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, dasl_fiber, type = "parquet")
+
+pin_name <- "dasl_fiber"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
