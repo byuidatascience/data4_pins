147c1b1da93a85a03f14f7a487c828e81dbaaaae
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-29 16:09:28 -0600

    done

---
+++ data_scripts/forced_expiratory_volume/forced_expiratory_volume.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(pins)
+
+forced_expiratory_volume <- read_csv('https://github.com/byuistats/data/raw/master/ForcedExpiratoryVolume/ForcedExpiratoryVolume.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, forced_expiratory_volume, type = "parquet")
+
+pin_name <- "forced_expiratory_volume"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
