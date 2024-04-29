f43796ecca09938f83aa95f475e48a0e4f71de20
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-29 16:26:48 -0600

    renamed everything to make both crocodile datasets plural

--- data_scripts/estuarine_crocodile/estuarine_crocodiles.R
+++ data_scripts/estuarine_crocodile/estuarine_crocodiles.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 library(pins)
 
-estuarine_crocodile <- read_csv('https://github.com/byuistats/data/raw/master/Estuarine_Crocodile_(Modified)/Estuarine_Crocodile_(Modified).csv')
+estuarine_crocodiles <- read_csv('https://github.com/byuistats/data/raw/master/Estuarine_Crocodile_(Modified)/Estuarine_Crocodile_(Modified).csv')
 
 
 # Publish the data to the server with Bro. Hathaway as the owner.
 board <- board_connect()
-pin_write(board, estuarine_crocodile, type = "parquet")
+pin_write(board, estuarine_crocodiles, type = "parquet")
 
-pin_name <- "estuarine_crocodile"
+pin_name <- "estuarine_crocodiles"
 meta <- pin_meta(board, paste0("hathawayj/", pin_name))
 client <- connect()
 my_app <- content_item(client, meta$local$content_id)
