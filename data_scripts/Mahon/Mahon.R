2fd27cc26fb25209186aa3fa3576fa1bdb9b3412
dustydells <98501262+dustydells@users.noreply.github.com>
2024-04-30 16:31:03 -0600

    fixed capitalization

--- data_scripts/Mahon/Mahon.R
+++ data_scripts/Mahon/Mahon.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(pins)
 library(connectapi)
 
-Mahon <- read_csv('https://github.com/byuistats/data/raw/master/Mahon/Mahon.csv')
+mahon <- read_csv('https://github.com/byuistats/data/raw/master/Mahon/Mahon.csv')
 
 
 # Publish the data to the server with Bro. Hathaway as the owner.
 board <- board_connect()
-pin_write(board, Mahon, type = "parquet", access_type = "all")
+pin_write(board, mahon, type = "parquet", access_type = "all")
 
-pin_name <- "Mahon"
+pin_name <- "mahon"
 meta <- pin_meta(board, paste0("hathawayj/", pin_name))
 client <- connect()
 my_app <- content_item(client, meta$local$content_id)
