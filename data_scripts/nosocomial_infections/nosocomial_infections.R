529c04c5382bfaf2432ac6c8f1479610a081af38
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-01 17:40:58 -0600

    done

---
+++ data_scripts/nosocomial_infections/nosocomial_infections.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(pins)
+library(connectapi)
+
+nosocomial_infections <- read_csv('https://github.com/byuistats/data/raw/master/NosocomialInfections/NosocomialInfections.csv') %>% 
+  select(!Description) # Delete description column. Information is in qmd.
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, nosocomial_infections, type = "parquet", access_type = "all")
+
+pin_name <- "nosocomial_infections"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
