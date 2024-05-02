53a600b60494d78cacfa41371e00b66ace6b0a9a
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-02 12:14:15 -0600

    done

---
+++ data_scripts/speed_of_light_by_michelson_1879/speed_of_light_by_michelson_1879.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(pins)
+library(connectapi)
+
+speed_of_light_by_michelson_1879 <- read_csv('')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, speed_of_light_by_michelson_1879, type = "parquet", access_type = "all")
+
+pin_name <- "speed_of_light_by_michelson_1879"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
