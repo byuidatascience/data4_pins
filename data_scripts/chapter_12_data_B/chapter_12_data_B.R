d8534d9cdf6564b54a59fc92fd813e7493d94107
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-02 17:28:44 -0600

    done, mysterious

--- data_scripts/chapter_12_data_B/chapter_12_data_B.R
+++ data_scripts/chapter_12_data_B/chapter_12_data_B.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(pins)
 library(connectapi)
 
-chapter_12_data_A <- read_csv('https://github.com/byuistats/data/raw/master/Chapter12DataA/Chapter12DataA.csv')
+chapter_12_data_B <- read_csv('https://github.com/byuistats/data/raw/master/Chapter12DataB/Chapter12DataB.csv')
 
 
 # Publish the data to the server with Bro. Hathaway as the owner.
 board <- board_connect()
-pin_write(board, chapter_12_data_A, type = "parquet", access_type = "all")
+pin_write(board, chapter_12_data_B, type = "parquet", access_type = "all")
 
-pin_name <- "chapter_12_data_A"
+pin_name <- "chapter_12_data_B"
 meta <- pin_meta(board, paste0("hathawayj/", pin_name))
 client <- connect()
 my_app <- content_item(client, meta$local$content_id)
