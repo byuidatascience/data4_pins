87d106934524b9a4791c08f15111c8e82669b742
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-02 12:40:29 -0600

    done

---
+++ data_scripts/survey_data/survey_data.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(pins)
+library(connectapi)
+
+survey_data <- read_csv('https://github.com/byuistats/data/raw/master/SurveyData/SurveyData.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, survey_data, type = "parquet", access_type = "all")
+
+pin_name <- "survey_data"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
