37cf72495bbbc70d3299ab40c9f90c8891791264
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-02 12:08:39 -0600

    done

---
+++ data_scripts/soviet_accidents/soviet_accidents.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(pins)
+library(connectapi)
+
+soviet_accidents <- read_csv('https://github.com/byuistats/data/raw/master/SovietAccidents/SovietAccidents.csv')
+
+
+# Publish the data to the server with Bro. Hathaway as the owner.
+board <- board_connect()
+pin_write(board, soviet_accidents, type = "parquet", access_type = "all")
+
+pin_name <- "soviet_accidents"
+meta <- pin_meta(board, paste0("hathawayj/", pin_name))
+client <- connect()
+my_app <- content_item(client, meta$local$content_id)
+set_vanity_url(my_app, paste0("data/", pin_name))
