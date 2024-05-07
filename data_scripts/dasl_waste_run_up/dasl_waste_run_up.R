c9c10abc05884d91a71f485b20979e3a766fce21
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-07 13:40:08 -0600

    better version of dataset now is grabbed from drive

--- data_scripts/dasl_waste_run_up/dasl_waste_run_up.R
+++ data_scripts/dasl_waste_run_up/dasl_waste_run_up.R
@@ -1,8 +1,12 @@
-library(tidyverse)
-library(pins)
-library(connectapi)
+pacman::p_load(tidyverse, pins, connectapi, googledrive, readxl)
 
-dasl_waste_run_up <- read_csv('https://github.com/byuistats/data/raw/master/DASL-WasteRunUp/DASL-WasteRunUp.csv')
+# Download the file from google drive (because the github copy only had one column)
+sdrive <- shared_drive_find("byuids_data") # This will ask for authentication.
+google_file <- drive_ls(sdrive) |>
+  filter(stringr::str_detect(name, "DASL-WasteRunUp"))
+tempf <- tempfile()
+drive_download(google_file, tempf)
+dasl_waste_run_up <- read_excel(tempf)
 
 
 # Publish the data to the server with Bro. Hathaway as the owner.
