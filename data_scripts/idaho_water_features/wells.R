b1aa62b43df7f55e72365310e98735a134e3637d
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-09 13:02:27 -0600

    error correction

--- data_scripts/idaho_water_features/wells.R
+++ data_scripts/idaho_water_features/wells.R
@@ -24,9 +24,9 @@ file_delete(exdir)
 
 # Publish the data to the server with Bro. Hathaway as the owner.
 board <- board_connect()
-pin_write(board, climate_change_antarctica, type = "parquet", access_type = "all")
+pin_write(board, wells, type = "parquet", access_type = "all")
 
-pin_name <- "climate_change_antarctica"
+pin_name <- "wells"
 meta <- pin_meta(board, paste0("hathawayj/", pin_name))
 client <- connect()
 my_app <- content_item(client, meta$local$content_id)
