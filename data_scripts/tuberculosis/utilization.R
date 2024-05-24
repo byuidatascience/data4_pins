6af883677eace664922a2ba3c9ea282ed142c3d7
dustydells <98501262+dustydells@users.noreply.github.com>
2024-05-24 13:25:20 -0600

    copyedits

--- data_scripts/tuberculosis/utilization.R
+++ data_scripts/tuberculosis/utilization.R
@@ -4,14 +4,6 @@ library(connectapi)
 
 tb_utilization <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=expenditure_utilisation")
 
-# Data details
-# dpr_document(tb_utilization, extension = ".md.R", export_folder = usethis::proj_get(),
-#              object_name = "tb_utilization", 
-#              title = "Word Health Organization (WHO) Tuberculosis expenditures and utilization by country",
-#              description = "See source for description of the data. tb_dictionary describes the column names.",
-#              source = "https://www.who.int/tb/country/data/download/en/",
-#              var_details = list(nothing = "nothing"))
-
 
 # Publish the data to the server with Bro. Hathaway as the owner.
 board <- board_connect()
